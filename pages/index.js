d534a2dcfa2410087860d0ed1409882c49b45812
davincecode <hyperscript.ca@gmail.com>
2022-06-06 00:08:29 -0600

    Update dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import Head from "next/head"
+import Container from "../components/container"
+import MoreStories from "../components/more-stories"
+import HeroPost from "../components/hero-post"
+import Intro from "../components/intro"
+import Layout from "../components/layout"
+import { getAllPostsForHome } from "../lib/api"
+import { PROJ_NAME } from "../lib/constants"
 
-export default function Index({ allPosts: { edges }, preview }) {
+export default function Index({ allPosts: { edges } }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
 
   return (
-    <Layout preview={preview}>
+    <Layout>
       <Head>
-        <title>Next.js Blog Example with {CMS_NAME}</title>
+        <title>{PROJ_NAME}</title>
       </Head>
       <Container>
         <Intro />
